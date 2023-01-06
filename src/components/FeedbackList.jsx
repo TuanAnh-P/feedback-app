93bf0cf3c3fd756497a4e277ce7ada2e77d7fe60
TuanAnh-P <1tuananhp@gmail.com>
2023-01-05 19:36:26 -0500

    Card Component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
 
 function FeedbackList({ feedback }) {
   
@@ -13,6 +14,16 @@ function FeedbackList({ feedback }) {
       <FeedbackItem key={item.id} item={item}/>
     ))}
   </div>
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
   )
 }
 
